ac0a1f4d281f02464f0c6d2811f05f75992055ba
Lilian.Pacaud <Lilian.Pacaud@sky-real.com>
2020-10-06 11:41:17 +0200

    Set port for prod

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const jwt = require('jsonwebtoken');  
 const cookieParser = require('cookie-parser');
-const port = 3000
+const port = process.env.PORT || 3000
 const router = express.Router();
 
 app.use(express.json());
